12e03b263b2e062bdc37bdd0b7ba09a4f73eb8b3
Lucia <luciaandreeaursu@yahoo.com>
2025-03-17 08:32:22 +0200

    add the Input component

---
+++ src/components/Input.jsx
@@ -0,0 +1,11 @@
+export default function Import({ placeHolder, value, onChange, className }){
+    return(
+        <input
+         type="text"
+         placeholder={placeHolder}
+         value={value}
+         onChange={onChange}
+         className={`p-2 w-full rounded-lg border border-gray-600 bg-gray-700 text-white focus:outline-none focus:ring-2 focus:ring-yellow-500 ${className}`}
+         />
+    )
+}
