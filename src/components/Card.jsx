64b805f8012379a8a663d56af3b9d79e0ac0d787
Lucia <luciaandreeaursu@yahoo.com>
2025-03-17 17:19:36 +0200

    updating the cards and adding margins

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,9 +3,13 @@ import { Link} from 'react-router-dom';
 export default function Card({id, image, title}){
     return(
         <Link to={`/recipe/${id}`} className="block" > 
-        <div className='p-4 rounded-lg shadow-md bg-gray-800 hover:shadow-lg transition'>
-            <img className='w-full h-40 object-cover rounded-lg' src={image} alt={title} />
-            <p className='text-center mt-2 text-white'></p>
+        <div className=' border-15 border-stone-400 bg-gray-800 rounded-lg overflow-hidden shadow-lg transform transition duration-300 hover:scale-105 w-70'>
+            <div className='relative'>
+            <img className=' w-70 h-55 object-cover  ' src={image} alt={title} />
+            <p className=' absolute bottom-0 bg-stone-400 bg-opacity-70 w-full text-white text-center text-lg py-0'>
+                {title}
+            </p>
+        </div>
         </div>
         </Link>
     )
