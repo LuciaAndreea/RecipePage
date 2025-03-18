6638bf040fb768c608e33bb8f9f637c52e3073b0
Lucia <luciaandreeaursu@yahoo.com>
2025-03-18 13:50:42 +0200

    update Select component

--- src/components/Select.jsx
+++ src/components/Select.jsx
@@ -1,7 +1,7 @@
 export default function Select({options, onChange, className}){
     return(
         <select
-        className={`p-2 rounded-lg border border-gray-600 bg-gray-700 text-white focus:outline-none focus:ring-2 focus:ring-yellow-500 ${className}`}
+        className={`p-2 rounded-lg border border-gray-600 bg-stone-700 text-white focus:outline-none focus:ring-2 focus:ring-yellow-500 ${className}`}
         onChange={onChange}>
             <option value="">All Categories</option>
             {options.map((option) =>(
