d98eb8e27cba0e3d4dfeea0ef249185cb856fc26
Lucia <luciaandreeaursu@yahoo.com>
2025-03-18 13:49:48 +0200

    update button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,7 @@ export default function Button({children, onClick, className}){
     return(
         <button 
         onClick={onClick}
-        className={`px-4 py-2 rounded-lg bg-yellow-500 text-black hover:bg-yellow-600 transition ${className}`}> 
+        className={`px-4 py-2 rounded-lg border border-gray-600 bg-stone-500 text-white 600 transition ${className}`}> 
         {children} 
         </button>
     )
