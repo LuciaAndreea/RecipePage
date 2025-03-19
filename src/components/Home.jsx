ce65c04708cd3e27b3e052da90d54043060769ef
Lucia <luciaandreeaursu@yahoo.com>
2025-03-19 10:09:41 +0200

    make the app's main page resposive on smaller displays

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -12,6 +12,7 @@ export default function Home(){
     const [search, setSearch] = useState("");
     const [selectedCategory, setSelectedCategory] = useState("Dessert");
     const [sortOrder, setSortOrder] = useState("asc");
+    const [menuOpen, setMenuOpen] = useState(false);
 
     useEffect(() =>{
         fetch("https://www.themealdb.com/api/json/v1/1/categories.php")
@@ -97,9 +98,27 @@ export default function Home(){
 
    return(
     <>
+    <nav className="bg-gray-900 text-white p-4 flex justify-between items-center md:hidden">
+      <h1 className="text-xl font-bold">Recipe App</h1>
+      <button onClick={() => setMenuOpen(!menuOpen)} className="text-2xl">🍔</button>
+    </nav>
+
+    {menuOpen && (
+      <div className="absolute top-0 left-0 w-64 h-full bg-gray-800 text-white p-4 z-50">
+        <button onClick={() => setMenuOpen(false)} className="text-xl mb-4">❌ Close</button>
+        <ul>
+          {categories.map((category, index) =>(
+            <li key={index} className="p-2 cursor-pointer hober:bg-gray-700" onClick={() =>{setSelectedCategory(category.name); setMenuOpen(false);}}>
+              {category.image && <img src={category.image} alt={category.name} className="w-6 h-6 inline-block mr-2"></img>}
+              {category.name}
+            </li>
+          ))}
+        </ul>
+      </div>
+    )}
     <div className="flex bg-neutral-900 min-h-screen text-white">
       {/* Sidebar cu categorii */}
-      <aside className="w-1/4 bg-neutral-800 flex flex-col items-center py-6">
+      <aside className="hidden md:block w-1/4 bg-gray-800 text-white p-4 min-h-screen">
       <div className="w-32 h-32 mb-6">
         <img src={logo} alt="Logo" className="w-full h-full object-cover rounded-full shadow-lg"/>
       </div>
@@ -155,7 +174,7 @@ export default function Home(){
         {loading ? (
           <p className="text-gray-400">Loading...</p>
         ) : recipes.length > 0 ? (
-          <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
             {sortedRecipes.map((recipe) => (
               <Card key={recipe.idMeal} id={recipe.idMeal} image={recipe.strMealThumb} title={recipe.strMeal} />
             ))}
