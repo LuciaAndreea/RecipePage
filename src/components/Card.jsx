0c911d89119c805856bef28efa2f26a439ec0917
Lucia <luciaandreeaursu@yahoo.com>
2025-03-19 18:42:45 +0200

    fix the card buttons

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,15 +2,18 @@ import { Link} from 'react-router-dom';
 
 export default function Card({id, image, title}){
     return(
-        <Link to={`/recipe/${id}`} className="block" > 
-        <div className=' border-15 border-neutral-800 bg-gray-800 rounded-lg overflow-hidden shadow-lg transform transition duration-300 hover:scale-105 w-70'>
-            <div className='relative'>
-            <img className=' w-70 h-55 object-cover  ' src={image} alt={title} />
-            <p className=' absolute bottom-0 bg-neutral-800 bg-opacity-70 w-full text-stone-300 text-center text-lg py-0'>
-                {title}
-            </p>
-        </div>
-        </div>
-        </Link>
+        <div className="border-15 border-neutral-800 bg-gray-800 rounded-lg overflow-hidden shadow-lg transform transition duration-300 hover:scale-105 w-70">
+  <div className="relative">
+    {/* Imaginea devine clicabilă */}
+    <Link to={`/recipe/${id}`}>
+      <img className="w-70 h-55 object-cover cursor-pointer" src={image} alt={title} />
+    </Link>
+    
+    {/* Titlul devine clicabil */}
+    <p className="absolute bottom-0 bg-neutral-800 bg-opacity-70 w-full text-stone-300 text-center text-lg py-0">
+      <Link to={`/recipe/${id}`} className="block w-full h-full py-2">{title}</Link>
+    </p>
+  </div>
+</div>
     )
 }
